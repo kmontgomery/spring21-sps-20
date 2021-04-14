9f47d9aacbd0b35a732f9a957dd07f92ba2ad45f
Makayla Price <makaylaj1121@gmail.com>
2021-04-14 16:05:07 +0000

    Add import line

--- src/main/java/com/google/sps/servlets/ResourceServlet.java
+++ src/main/java/com/google/sps/servlets/ResourceServlet.java
@@ -14,15 +14,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
+import com.google.sps.data.Location;
 
 @WebServlet("/resource")
 public class ResourceServlet extends HttpServlet {
     @Override
     public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         response.setContentType("text/html;");
+        response.getWriter().println("<h1>News and Resources from Your Area!</h1>");
         response.getWriter().println("https://www.nytimes.com/2021/03/18/nyregion/asian-hate-crimes.html");
-        response.getWriter().println(
-                "https://theconversation.com/african-american-teens-face-mental-health-crisis-but-are-less-likely-than-whites-to-get-treatment-140697");
+        response.getWriter().println("https://theconversation.com/african-american-teens-face-mental-health-crisis-but-are-less-likely-than-whites-to-get-treatment-140697");
         response.getWriter().println("https://www.plannedparenthood.org/");
 
         final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
@@ -33,21 +34,24 @@ public class ResourceServlet extends HttpServlet {
 
         while (results.hasNext()) {
             final Entity entity = results.next();
-            final String resource = entity.getString("text");
+            final String resource = entity.getString("title");
+            response.getWriter().println("<li>" + resource + "</li>");
+            
         }
     }
 
     @Override
-    public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
-        final String title = request.getParameter("location");
-        final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-        final KeyFactory keyFactory = datastore.newKeyFactory().setKind("Location");
-        final FullEntity locationEntity = Entity.newBuilder(keyFactory.newKey())
-            .set("title", title)
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String title = request.getParameter("location");
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Location");
+        FullEntity locationEntity = Entity.newBuilder(keyFactory.newKey())
+            .set("location", title)
             .set("timestamp", System.currentTimeMillis())
             .build();
+        System.out.println(locationEntity);
         datastore.put(locationEntity);
 
-        response.sendRedirect("/resource");
+        response.sendRedirect("/index.html");
     }
 }
